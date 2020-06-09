3a030839394d4080763fcd46d7d7d40ef9e898c8
harshitmittal99 <46072196+harshitmittal99@users.noreply.github.com>
2020-06-09 17:30:15 +0530

    Update deck.py

--- deck.py
+++ deck.py
@@ -1,51 +1,49 @@
+import abc
+
 import numpy as np
 
-import abc
+
 class deck(metaclass=abc.ABCMeta):
     def __init__(self):
-        self.arr = np.arange(1,53,1)
-    
-    def Shuffle(self):
-        arr =self.arr
-        np.random.shuffle(arr)
-        self.arr = arr
-
-    def Method1111(self):
-        divisions = [[],[],[],[]]
-        for i in range (52):
-            divisions[i%4].append(self.arr[i])
-        return divisions
-    
-    def Method1313(self):
-        divisions = [[],[],[],[]]
-        for i in range (52):
-            divisions[int(i//13)].append(self.arr[i])
-        return divisions
-    
-    def Method544(self):
-        divisions = [[],[],[],[]]
-        for i in range (20):
-            divisions[i//5].append(self.arr[i])
-        for i in range (20,36):
-            divisions[(i-20)//4].append(self.arr[i])
-        for i in range (36,52):
-            divisions[(i-36)//4].append(self.arr[i])
-        return divisions
+        self.arr = np.arange(1, 53, 1)
 
     @abc.abstractmethod
-    def func(self):
+    def Shuffle(self):
+        pass
+
+    def Distribute(self):
         pass
 
 
 class Deck(deck):
-    def func(self):
-        pass
+    def Shuffle(self):
+        arr = self.arr
+        np.random.shuffle(arr)
+        self.arr = arr
+
+    def Distribute(self, distributeMethod):
+        divisions = [[], [], [], []]
+        if distributeMethod == "1111":
+            for i in range(52):
+                divisions[i % 4].append(self.arr[i])
+            return divisions
+
+        elif distributeMethod == "1313":
+            for i in range(52):
+                divisions[int(i//13)].append(self.arr[i])
+            return divisions
+        elif distributeMethod == "544":
+            for i in range(20):
+                divisions[i//5].append(self.arr[i])
+            for i in range(20, 36):
+                divisions[(i-20)//4].append(self.arr[i])
+            for i in range(36, 52):
+                divisions[(i-36)//4].append(self.arr[i])
+            return divisions
 
 
 deck1 = Deck()
 deck1.Shuffle()
-print(deck1.Method1111())
-print(deck1.Method1313())
-print(deck1.Method544())
-
-
+print(deck1.Distribute("1111"))
+print(deck1.Distribute("1313"))
+print(deck1.Distribute("544"))
