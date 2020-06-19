28b60484ffa5e97412d2ac767b33f2514df8d2a9
harshitmittal99 <144harshit@gmail.com>
2020-06-19 23:17:15 +0530

    flake8 correction

--- deck.py
+++ deck.py
@@ -1,15 +1,15 @@
-import itertools
 from enum import Enum
 from random import shuffle
-class Deck:
 
+
+class Deck:
     class DistributionStrategy(Enum):
         OneOne = 1
         OneThree = 2
         FiveFourFour = 3
 
     def __init__(self):
-        self.cards = list(i for i in range (1,53,1))
+        self.cards = list(i for i in range(1, 53, 1))
         self.distribution_strategies = {
             Deck.DistributionStrategy.OneOne: self.one_one,
             Deck.DistributionStrategy.OneThree: self.one_three,
@@ -30,7 +30,7 @@ class Deck:
 
     def five_four_four(self):
         return list(
-            self.cards[i * 5: (i + 1) * 5]+
-            self.cards[20 + (i * 4): 24 + (i * 4)]+
+            self.cards[i * 5: (i + 1) * 5] +
+            self.cards[20 + (i * 4): 24 + (i * 4)] +
             self.cards[36 + (i * 4): 40 + (i * 4)]
-         for i in range(4))
+            for i in range(4))
